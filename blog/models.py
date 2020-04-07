718eaa0339b547150c109a041ec959b0ccb66b23
Alex <alx.yurkin@gmail.com>
2020-04-07 13:44:52 +0300

    add save posts as drafts, delete posts

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200)
